67ceccd5e975d915a342c390ce75e01a85f0f697
JoaoBarreto35 <143046404+JoaoBarreto35@users.noreply.github.com>
2026-06-24 09:21:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 import os
 from google import genai
 from google.genai import types
+import requests
 
 app = FastAPI()
 security = HTTPBearer()
@@ -35,7 +36,25 @@ def minha_funcao_original(api_key,modelo,CONTEXTO_CLASSIFICACAO,texto):
 
     except Exception as e:
 
-        return False, f"{e}"
+        return False, e
+    
+def bridge(url, token):
+    headers = {"Authorization": f"Basic {token}"}
+
+    try:
+        response = requests.get(url, headers=headers, timeout=20)
+        if response.status_code == 200:
+            data = response.json().get("data", [])
+            return data
+        elif response.status_code in [401,403]:
+            return []
+        else:
+            return []
+    except:
+        return []
+    
+
+
 
 @app.get("/api/executar")
 def executar_funcao(api_key,modelo,CONTEXTO_CLASSIFICACAO,texto, credentials: HTTPAuthorizationCredentials = Security(security)):
@@ -47,4 +66,15 @@ def executar_funcao(api_key,modelo,CONTEXTO_CLASSIFICACAO,texto, credentials: HT
         )
     
     resultado = minha_funcao_original(api_key,modelo,CONTEXTO_CLASSIFICACAO,texto)
+    return {"resultado": resultado}
+
+@app.get("/api/bridge")
+def executar_bridge(url,token, credentials: HTTPAuthorizationCredentials = Security(security)):
+    if credentials.credentials != API_SECRET_TOKEN:
+        raise HTTPException(
+            status_code=401, 
+            detail="Token inválido ou não fornecido"
+        )
+    
+    resultado = bridge(url,token)
     return {"resultado": resultado}
